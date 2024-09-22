723418de40caa8dd66258d1600cabc66f7265255
Matt Paige <mattpaige@HWs-MacBook-Pro.hsd1.ga.comcast.net>
2024-09-22 18:25:28 -0400

    Firebase added

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD8q1PALM_qCh457xWa0HoYQKTU6tNxwaI",
+  authDomain: "corbinbrownvideo.firebaseapp.com",
+  projectId: "corbinbrownvideo",
+  storageBucket: "corbinbrownvideo.appspot.com",
+  messagingSenderId: "988994065389",
+  appId: "1:988994065389:web:ada63864ce7d7df2d64de4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
